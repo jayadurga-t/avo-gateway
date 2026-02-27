726e9b5b9227dbd34aa5dc3d7972a7fac1afb82c
jayadurga-t <jayadurga.t332@gmail.com>
2026-02-27 23:36:17 +0530

    feat(gateway): add resilience4j circuit breaker with fallback support

--- gatewayserver/src/main/java/com/avo/gatewayserver/GatewayserverApplication.java
+++ gatewayserver/src/main/java/com/avo/gatewayserver/GatewayserverApplication.java
@@ -2,12 +2,14 @@ package com.avo.gatewayserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.filter.factory.SpringCloudCircuitBreakerFilterFactory;
 import org.springframework.cloud.gateway.route.Route;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.*;
 import org.springframework.context.annotation.Bean;
 
 import java.time.LocalDateTime;
+import java.util.function.Consumer;
 import java.util.function.Function;
 
 @SpringBootApplication
@@ -29,7 +31,13 @@ public class GatewayserverApplication {
 														@Override
 														public UriSpec apply(GatewayFilterSpec gatewayFilterSpec) {
 															return gatewayFilterSpec.rewritePath("/avobank/accounts/(?<segment>.*)","/${segment}")
-																					.addResponseHeader("X-Response-Time", LocalDateTime.now().toString());
+																					.addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
+																					.circuitBreaker(new Consumer<SpringCloudCircuitBreakerFilterFactory.Config>() {
+																						@Override
+																						public void accept(SpringCloudCircuitBreakerFilterFactory.Config config) {
+																							config.setName("accountsCircuitBreaker").setFallbackUri("forward:/contactSupport");
+																						}
+																					});
 														}
 													})
 													.uri("lb://ACCOUNTS");
@@ -44,7 +52,13 @@ public class GatewayserverApplication {
 														@Override
 														public UriSpec apply(GatewayFilterSpec gatewayFilterSpec) {
 															return gatewayFilterSpec.rewritePath("/avobank/cards/(?<segment>.*)","/${segment}")
-																					.addResponseHeader("X-Response-Time", LocalDateTime.now().toString());
+																					.addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
+																					.circuitBreaker(new Consumer<SpringCloudCircuitBreakerFilterFactory.Config>() {
+																						@Override
+																						public void accept(SpringCloudCircuitBreakerFilterFactory.Config config) {
+																							config.setName("cardsCircuitBreaker").setFallbackUri("forward:/contactSupport");
+																						}
+																					});
 														}
 													})
 													.uri("lb://CARDS");
@@ -59,7 +73,13 @@ public class GatewayserverApplication {
 														@Override
 														public UriSpec apply(GatewayFilterSpec gatewayFilterSpec) {
 															return gatewayFilterSpec.rewritePath("/avobank/loans/(?<segment>.*)","/${segment}")
-																					.addResponseHeader("X-Response-Time", LocalDateTime.now().toString());
+																					.addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
+																					.circuitBreaker(new Consumer<SpringCloudCircuitBreakerFilterFactory.Config>() {
+																						@Override
+																						public void accept(SpringCloudCircuitBreakerFilterFactory.Config config) {
+																							config.setName("loansCircuitBreaker").setFallbackUri("forward:/contactSupport");
+																						}
+																					});
 														}
 													})
 													.uri("lb://LOANS");
