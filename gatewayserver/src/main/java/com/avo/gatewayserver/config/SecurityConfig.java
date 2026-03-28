72fda614d1ecc9845726f793477fa4386e96e321
jayadurga-t <jayadurga.t332@gmail.com>
2026-03-28 22:27:48 +0530

    feat(security): add role-based authorization using Keycloak roles

--- gatewayserver/src/main/java/com/avo/gatewayserver/config/SecurityConfig.java
+++ gatewayserver/src/main/java/com/avo/gatewayserver/config/SecurityConfig.java
@@ -2,11 +2,17 @@ package com.avo.gatewayserver.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverterAdapter;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import reactor.core.publisher.Mono;
 
 @Configuration
 @EnableWebFluxSecurity
@@ -18,15 +24,20 @@ public class SecurityConfig {
             @Override
             public void customize(ServerHttpSecurity.AuthorizeExchangeSpec authorizeExchangeSpec) {
                 authorizeExchangeSpec.pathMatchers(HttpMethod.GET).permitAll()
-                        .pathMatchers("/avobank/accounts/**").authenticated()
-                        .pathMatchers("avobank/cards/**").authenticated()
-                        .pathMatchers("/avobank/loans/**").authenticated();
+                        .pathMatchers("/avobank/accounts/**").hasRole("ACCOUNTS")
+                        .pathMatchers("avobank/cards/**").hasRole("CARDS")
+                        .pathMatchers("/avobank/loans/**").hasRole("LOANS");
             }
         })
                 .oauth2ResourceServer(new Customizer<ServerHttpSecurity.OAuth2ResourceServerSpec>() {
                     @Override
                     public void customize(ServerHttpSecurity.OAuth2ResourceServerSpec oAuth2ResourceServerSpec) {
-                        oAuth2ResourceServerSpec.jwt(Customizer.withDefaults());
+                        oAuth2ResourceServerSpec.jwt(new Customizer<ServerHttpSecurity.OAuth2ResourceServerSpec.JwtSpec>() {
+                            @Override
+                            public void customize(ServerHttpSecurity.OAuth2ResourceServerSpec.JwtSpec jwtSpec) {
+                                jwtSpec.jwtAuthenticationConverter(grantedAuthoritiesExtractor());
+                            }
+                        });
                     }
                 });
         serverHttpSecurity.csrf(new Customizer<ServerHttpSecurity.CsrfSpec>() {
@@ -36,6 +47,14 @@ public class SecurityConfig {
             }
         });
         return serverHttpSecurity.build();
+    }
+
+    public Converter<Jwt, Mono<AbstractAuthenticationToken>> grantedAuthoritiesExtractor(){
+        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new KeyCloakRoleConverter());
+
+        return new ReactiveJwtAuthenticationConverterAdapter(jwtAuthenticationConverter);
     }
 
 }
